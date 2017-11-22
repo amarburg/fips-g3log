e6f1d3eaf7a92080beac6c93b0a37a4494e0c35e
Aaron Marburg <amarburg@apl.washington.edu>
2017-11-22 15:00:48 -0800

    Removing the explicit FIPS_EXCEPTION, it needs to be provided in the configuration.

--- fips-include.cmake
+++ fips-include.cmake
@@ -1,6 +1,2 @@
-# Enabled exceptions...
-message( STATUS   "fips-G3log requires exceptions, enabling....")
-set(FIPS_EXCEPTIONS on CACHE BOOL  "" FORCE)
-
 ## Allows g3log/generated_definitions.hpp to be found
 fips_include_directories( ${FIPS_PROJECT_BUILD_DIR}/include/ )
