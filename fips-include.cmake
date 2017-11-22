067767c81088accca3493a9de5d89cff11e00310
Aaron Marburg <amarburg@apl.washington.edu>
2017-11-22 14:14:47 -0800

    Changed FIPS_EXCEPTIONS setting to a CACHE FORCE setting

--- fips-include.cmake
+++ fips-include.cmake
@@ -1,6 +1,6 @@
 # Enabled exceptions...
-# message( STATUS   "fips-G3log requires exceptions, enabling....")
-# set(FIPS_EXCEPTIONS on)
+message( STATUS   "fips-G3log requires exceptions, enabling....")
+set(FIPS_EXCEPTIONS on CACHE BOOL  "" FORCE)
 
 ## Allows g3log/generated_definitions.hpp to be found
 fips_include_directories( ${FIPS_PROJECT_BUILD_DIR}/include/ )
